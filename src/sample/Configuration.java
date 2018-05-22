d769cc3b697d6359a87683b4ebfb2192bf0985ab
Happy <pmp0606@mail.ru>
2018-05-22 20:15:29 +0300

    Initial Commit

---
+++ src/sample/Configuration.java
@@ -0,0 +1,19 @@
+package sample;
+
+public interface Configuration {
+
+    String USER_TABLE = "costs";
+    String ID = "id";
+    String NAME_OF_ADV = "name_of_adv";
+    String NAME_OF_PARFUME = "name_of_parfume";
+    String PRICE_OF_ADV = "price_of_adv";
+    String DATE_OF_ADV = "date_of_adv";
+    String IS_PAID = "is_paid";
+
+    String DBHOST = "localhost";
+    String DBPORT = "3306";
+    String DBUSER = "root";
+    String DBPASS = "root";
+    String DBNAME = "perfume";
+
+}
