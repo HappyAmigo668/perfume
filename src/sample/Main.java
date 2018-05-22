029284c8b25371c1444f5fd2d6269e66635a584c
Gleb Remniov <OdinRossy@users.noreply.github.com>
2018-05-22 19:30:11 +0300

    Commit by Gleb

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,10 +11,9 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("fxmls/sample.fxml"));
         primaryStage.setTitle("Расходы на рекламу");
-        primaryStage.setScene(new Scene(root, 700, 400));
+        primaryStage.setScene(new Scene(root, 800, 500));
         primaryStage.show();
     }
-
     public static void main(String[] args) {
         launch(args);
     }
